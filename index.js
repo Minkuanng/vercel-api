8d045bf6f3a928789a7252ebfe6ee7001361dee9
Minkuan <qn781159@gmail.com>
2026-07-17 03:27:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // index.js - Root directory
 module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   res.status(200).json({
     message: 'API is running!',
     endpoints: {
