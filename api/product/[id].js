86f74c14ab3b1fcd7be910e6364b5dacca2b268f
Minkuan <qn781159@gmail.com>
2026-07-17 03:15:48 +0700

    Update [id].js

--- api/product/[id].js
+++ api/product/[id].js
@@ -3,14 +3,18 @@ const admin = require('firebase-admin');
 const { getFirestore } = require('firebase-admin/firestore');
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-  });
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+      }),
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+    });
+  } catch (error) {
+    console.error('Firebase initialization error:', error);
+  }
 }
 
 const db = getFirestore();
@@ -43,7 +47,7 @@ module.exports = async (req, res) => {
 
     return res.status(200).json({
       success: true,
-      data: productDoc.data()
+      data: { id: productDoc.id, ...productDoc.data() }
     });
 
   } catch (error) {
