72bb19cf69f303861e539416203a921147d42e35
Minkuan <qn781159@gmail.com>
2026-07-17 11:52:35 +0700

    Delete api directory

--- api/hello.js
+++
@@ -1,9 +0,0 @@
-const { withCors } = require('../lib/firebase');
-
-module.exports = withCors(async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'API đang hoạt động ✅',
-    time: new Date().toISOString(),
-  });
-});
